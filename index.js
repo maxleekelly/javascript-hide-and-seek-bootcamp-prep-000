4bff45ce9d4f8b81226b05a5e01a604c5d34d127
Max Kelly <maxleekelly@gmail.com>
2018-01-24 19:41:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n);
+  }
+}
+
+function deepestChild() {
+  var grand = document.querySelector("#grand-node");
+  while (grand.children[0]) {
+    grand = grand.children[0];
+  }
+  return grand;
+}
