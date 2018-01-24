ae1c307ac42e1515cd0f2ada152d10393a327ee0
Max Kelly <maxleekelly@gmail.com>
2018-01-24 19:38:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n);
+  }
+}
+
+function deepestChild() {
+  var grand = document.querySelector("#grand-node");
+  
+}
